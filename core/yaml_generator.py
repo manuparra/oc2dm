7a15aa735ce6dd004fd947a07e0849a71ea0c74f
Mancuerna <jacortesibanez@gmail.com>
2017-05-08 20:15:20 +0200

    First working version.

--- core/yaml_generator.py
+++ core/yaml_generator.py
@@ -1,52 +1,19 @@
 from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
 import yaml
+import sys
+import ruamel.yaml
+import collections
+
+sys.path.insert(0, '../experimental/queringjsonld/')
+from queryexample import turtle_query
 
 class YamlGenerator:
-
+    ruamel.yaml.representer.RoundTripRepresenter.add_representer(collections.OrderedDict, ruamel.yaml.representer.RoundTripRepresenter.represent_ordereddict)
     def __init__(self, input_file, output_file):
-        service_def=open("../services_definition/turtle/{}".format(input_file)).read()
-
-        g = Graph().parse(data=service_def, format='turtle')
-
-        qres_base = g.query(
-            """PREFIX dmmlcc: <http://dicits.ugr.es/dmmlcc#>
-            PREFIX waa: <http://purl.oclc.org/NET/WebAuthentication> 
-            PREFIX mls: <http://www.w3.org/ns/mls> 
-            SELECT  ?mlservice ?mldescription ?mlcreator ?mlcreated ?mlauthtype ?descrinput ?descrdataset ?datafilenamemandatory
-                WHERE { 
-                    ?mlservice dmmlcc:hasOperation ?b .
-                    ?mlservice dcterms:description ?mldescription .
-                    ?mlservice dcterms:creator ?mlcreator .
-                    ?mlservice dcterms:created ?mlcreated .            
-                    ?mlservice dmmlcc:hasAuthentication ?c .
-                    ?c waa:requiresAuthentication ?mlauthtype .
-                    ?b mls:hasInput ?input .
-                    ?input dcterms:description ?descrinput .
-                    ?input dmmlcc:contains ?contains .
-                    ?contains mls:Data ?data .
-                    ?data mls:DataSet ?dataset . 
-                    ?dataset dcterms:description ?descrdataset .
-                    ?dataset dmmlcc:datafilename ?datafilename .
-                    ?dataset dmmlcc:mandatory ?datafilenamemandatory .
-                }
-            """)
-
-        qres_inputparams = g.query(
-            """PREFIX dmmlcc: <http://dicits.ugr.es/dmmlcc#>
-            PREFIX waa: <http://purl.oclc.org/NET/WebAuthentication> 
-            PREFIX mls: <http://www.w3.org/ns/mls> 
-            SELECT  ?params  ?description ?mandatory ?defaultvalue
-            WHERE { 
-                    ?mlservice dmmlcc:hasInputParameters ?mlserviceinputparameters .
-                    ?mlserviceinputparameters dmmlcc:Parameters ?params .
-                    ?params dmmlcc:mandatory ?mandatory .
-                    ?params dcterms:description ?description .
-                    ?params dmmlcc:defaultvalue ?defaultvalue .
-                }
-            """)
-       
+        qres_base, qres_inputparams = turtle_query(input_file)
         parameters = []
+
         for row in qres_inputparams:
             name = row[0][:]
             description = row[1][:]
@@ -55,8 +22,18 @@ class YamlGenerator:
             else:
                 required = True
             default = row[3][:]
-            parameters.append({'name':name, 'description':description, 'required':required, 'default':default})
-        print (yaml.dump(parameters, default_flow_style=False, indent=2)) 
-       # stream = open('document.yaml', 'w')
-       # yaml.dump(parameters, stream, default_flow_style=False, indent=2)
-YamlGenerator("lr.ttl", "asd")
+            parameters.append({'in':'query', 'name':name, 'description':description, 'required':required, 'default':default})
+        data = collections.OrderedDict([('swagger', '2.0'),
+                ('title', 'OPENCCML API'),
+                ('version', '0.1'),
+                ('consumes', ['application/json']),
+                ('produces', ['application/json']),
+                ('paths', {'/linear_regression': {'get': {'operationId': 'linear_regression.execute',
+                                          'parameters': parameters,
+                                          'type':'array',
+                                          'summary': 'Execute a linear regression over the provided dataset',
+                                          'tags': ['Method']}}})])
+        out_file = open('base.yml', 'w')
+        ruamel.yaml.dump(data, out_file, default_flow_style=False, Dumper=ruamel.yaml.RoundTripDumper, indent=2)
+
+YamlGenerator("cor.ttl", "asd")
