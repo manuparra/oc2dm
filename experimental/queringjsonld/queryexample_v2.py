7e9d5daccb0d6786132e331198472ddf06613bb0
ManuelParra <manuparra@gmail.com>
2017-05-09 22:58:24 +0200

    Serialising SparQL to JSON

--- experimental/queringjsonld/queryexample_v2.py
+++ experimental/queringjsonld/queryexample_v2.py
@@ -118,14 +118,19 @@ class SPARQL_driver:
         
         self.service_file=open("{}".format(turtle_file)).read()
         self.graph = Graph().parse(data=self.service_file, format='turtle')
+        self.base=None
+        self.auth=None
+        self.input=None
+        self.output=None
+        self.implementation=None
                 
     def _extract_base(self):
-        print "-----"
-        self.base = self.graph.query(
+        
+        query_results = self.graph.query(
             """ PREFIX dmmlcc: <http://dicits.ugr.es/dmmlcc#>
                 PREFIX waa: <http://purl.oclc.org/NET/WebAuthentication> 
                 PREFIX mls: <http://www.w3.org/ns/mls> 
-                SELECT  ?mlservice ?mldescription ?mlcreator ?mlmodified ?mlpublisher ?mltitle ?mlcomments 
+                SELECT  ?mlservice  ?mldescription ?mlcreator ?mlmodified ?mlpublisher ?mltitle ?mlcomments 
                     WHERE { 
                         ?mlservice dmmlcc:hasOperation ?b .
                         ?mlservice dcterms:description ?mldescription .
@@ -134,20 +139,45 @@ class SPARQL_driver:
                         ?mlservice dcterms:modified ?mlmodified .
                         ?mlservice dcterms:publisher ?mlpublisher .
                         ?mlservice dcterms:title ?mltitle .
-                        ?mlservice dcterms:comments ?mlcomments .
+                        ?mlservice rdfs:comments ?mlcomments .
                     }
             """)
-        for b in self.base:
-            print("--AMLService: %s\n" \
-                "MLDescription: %s\n" \
-                "MLCreator: %s\n" \
-                "MLCreated: %s\n" \
-                "MLAuthType: %s\n" \
-                "MLInputDescription: %s\n" \
-                "MLDataSetDescription %s\n" \
-                "MLDataSetMandatory: %s\n" % b) 
+        
+        self.base=query_results.serialize(format="json")
+          
+    def _extract_authentication(self):
+        
+        query_results = self.graph.query(
+            """ PREFIX dmmlcc: <http://dicits.ugr.es/dmmlcc#>
+                PREFIX waa: <http://purl.oclc.org/NET/WebAuthentication> 
+                PREFIX mls: <http://www.w3.org/ns/mls> 
+                SELECT  ?mlservice ?mlauthentication ?mlauthtype ?mlauthdescription
+                    WHERE { 
+                        ?mlservice dmmlcc:hasAuthentication ?mlauthentication .
+                        ?mlauthentication waa:requiresAuthentication ?mlauthtype .
+                        ?mlauthentication dcterms:description ?mlauthdescription .
+                    }
+            """)
+        
+        self.auth=query_results.serialize(format="json")
+        
     def _extract_input(self):
-        pass
+        query_results = self.graph.query(
+                 """PREFIX dmmlcc: <http://dicits.ugr.es/dmmlcc#>
+                 PREFIX waa: <http://purl.oclc.org/NET/WebAuthentication> 
+                 PREFIX mls: <http://www.w3.org/ns/mls> 
+                 SELECT  ?mlservice ?dataset ?features
+                     WHERE { 
+                         ?mlservice dmmlcc:hasOperation ?b .
+                         ?b mls:hasInput ?input .
+                         ?input dmmlcc:contains ?contains .
+                         ?contains mls:Data ?data .
+                         ?data mls:Feature ?features .
+                        }
+            """)
+        
+        self.input=query_results.serialize(format="json")
+        print self.input
     def _extract_output(self):
         pass
     def _extract_implementation(self):
@@ -164,5 +194,5 @@ for row in params:
 
 sparQL=SPARQL_driver(turtle_file="../../services_definition/turtle/cor.ttl")
 
-sparQL._extract_base()
+sparQL._extract_input()
         
