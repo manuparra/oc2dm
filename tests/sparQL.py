ae2bb4dfe324aa8b425be7e581fe7f3377315d21
Manuel Parra <manuparra@gmail.com>
2017-05-21 00:43:19 +0200

    Updated test SparQL

--- tests/sparQL.py
+++ tests/sparQL.py
@@ -1,12 +1,25 @@
 import unittest
+import json
 
 from sparql_parser.sparql_parser import SPARQL_driver
 
 class TestSparQLLibrary(unittest.TestCase):
 
-    def test_readallservices(self):
+    def test_base_service_vars(self):
         sqld=SPARQL_driver(turtle_file="linear_regression.ttl",test=True)
-        self.assertEqual('foo'.upper(), 'FOO')
+        sqld._extract_base()
+        # Convert to Dict
+        dictjson=json.loads(sqld.base)
+        self.assertTrue(len(dictjson['head']['vars'])>0 and len(dictjson['results']['bindings'])>0)
+
+    def test_base_service_binding(self):
+        sqld=SPARQL_driver(turtle_file="linear_regression.ttl",test=True)
+        sqld._extract_base()
+        # Convert to Dict
+        dictjson=json.loads(sqld.base)
+        self.assertTrue(len(dictjson['results']['bindings'])>0)
+
+
 
 
 if __name__ == '__main__':
