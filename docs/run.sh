2e1d637a49fb44fc198c109fc4243adb837e4e77
ManuelParra <manuparra@gmail.com>
2017-05-23 20:06:28 +0200

    readme updated: how to

--- docs/run.sh
+++ docs/run.sh
@@ -1,11 +1,18 @@
 # Documentation generator in one step
 
-echo "--> HTML Documentation"
-make html
-echo "--> Creating output folder of the documentation"
-mkdir -p ./html/
-echo "--> Copying output documentation HTML folder"
-cp -r ./build/html/* ./html
+output=$1
+echo "----------------------------------------------"
+echo " HTML Documentation"
+echo "----------------------------------------------"
+make $1
+echo "----------------------------------------------"
+echo " Creating output folder of the documentation"
+echo "----------------------------------------------"
+mkdir -p ./$1/
+echo "----------------------------------------------"
+echo " Copying output documentation HTML folder"
+echo "----------------------------------------------"
+cp -r ./build/$1/* ./$1
 echo "----------------------------------------------"
 echo "Documentation of OpenCCML is stored in ./html/"
 echo "----------------------------------------------"
