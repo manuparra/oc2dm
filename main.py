3776e52c68808f6c9e2b1b2633e5e65205ada648
Mancuerna <jacortesibanez@gmail.com>
2017-06-23 12:35:11 +0200

    Added "wrapperR" folder.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from sys import path
 
 from falsy.falsy import FALSY
 
-from config.config import turtle_folder, catalog_folder, sparql_parser_folder
+from config.config import turtle_folder, catalog_folder, sparql_parser_folder, wrapperR_folder
 from core.yaml_generator import YamlGenerator
 
 path.append(sparql_parser_folder)
+path.append(wrapperR_folder)
 yml = YamlGenerator(turtle_folder, catalog_folder)
 f = FALSY(static_dir='/core/static')
 f.swagger('catalog/catalog.yml', ui=True, theme='impress')
