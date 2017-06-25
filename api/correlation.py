9e79ad7f9701fe1f8162353dd885a93da2115639
Mancuerna <jacortesibanez@gmail.com>
2017-06-25 20:02:39 +0200

    Download service definitions.

--- api/correlation.py
+++ api/correlation.py
@@ -6,3 +6,13 @@ def execute(use):
 def execute_post(use):
     data = "no data here"
     return {'data': data}
+
+
+def download():
+    with open("linear_regression.ttl") as file:
+        return file
+
+
+def download_post():
+    with open("linear_regression.ttl") as file:
+        return file
