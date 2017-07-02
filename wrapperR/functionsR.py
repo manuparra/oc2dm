90c761bf8ddf604c77368537588b70b077b62425
rubenCastro <rubencastro07@gmail.com>
2017-07-03 01:00:28 +0200

    Split R functions

---
+++ wrapperR/functionsR.py
@@ -0,0 +1,18 @@
+import rpy2.robjects as ro
+
+def lm (objectCore):
+	lm = ro.r("""
+		library("r2pmml")
+		dataset = read.csv(file="{0}", header = TRUE, sep=',')
+	    resultfit = lm({1}, data=dataset)
+	    r2pmml(resultfit,file="{2}")
+    """.format(objectCore.parameter.dataset['ruta'], objectCore.parameter.parameters, objectCore.parameter.outputPMML))
+
+def cor (objectCore):
+	cor = ro.r("""
+		library("r2pmml")
+		dataset = read.csv(file="{0}", header = TRUE, sep=',')
+	    resultfit = cor({1})
+	    saveRDS(resultfit, "{2}")
+	    print(resultfit)
+    """.format(objectCore.parameter.dataset['ruta'], objectCore.parameter.parameters, objectCore.parameter.outputPMML))
--- wrapperR/wrapperv2.py
+++ wrapperR/wrapperv2.py
@@ -15,46 +15,21 @@
 
 """RWrapper"""
 
-import csv
-import rpy2.robjects as ro
 from dataset import *
-from rpy2.robjects.packages import importr
-from rpy2.robjects import pandas2ri
-import pandas as pd
-
+from functionsR import *
 
 class core:
-
-	"""
-	Package CORE of R. Includes methods from the CORE of R, and not included on additional Packages
-	"""
-	
 
 	def __init__(self, dictionary, method):
 		self.parameter = Dataset(dictionary, method)
 	
-	def lm (self):
-		lm = ro.r("""
-			library("r2pmml")
-			dataset = read.csv(file="{0}", header = TRUE, sep=',')
-	        resultfit = lm({1}, data=dataset)
-	        r2pmml(resultfit,file="{2}")
-     	""".format(self.parameter.dataset['ruta'], self.parameter.parameters, self.parameter.outputPMML))
-
-	def cor (self):
-		cor = ro.r("""
-			library("r2pmml")
-			dataset = read.csv(file="{0}", header = TRUE, sep=',')
-	        resultfit = cor({1})
-	        saveRDS(resultfit, "{2}")
-	        print(resultfit)
-     	""".format(self.parameter.dataset['ruta'], self.parameter.parameters, self.parameter.outputPMML))
-
-parametros = {'x': 'dataset$mpg', 'y': 'dataset$disp', 'method': 'spearman', 'dataset': 'mtcars.csv'}
+	
+'''parametros = {'x': 'dataset$mpg', 'y': 'dataset$disp', 'method': 'spearman', 'dataset': 'mtcars.csv'}
+dataset = core(parametros, "cor")
+cor(dataset)'''
 
 #parametros = {'na__action':'na.omit', 'dataset': 'mtcars.csv', 'formula': 'mpg~disp', 'weights': 'NULL', 'subset': 'NULL'}
-dataset = core(parametros, "cor")
-dataset.cor()
+#dataset.cor()
 #entrada = {'na__action':'na.omit', 'dataset': 'mtcars.csv', 'formula': 'mpg~disp', 'weights': 'NULL', 'subset': 'NULL'}
 #p = core(entrada)
 #p.lm()	
