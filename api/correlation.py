1a2ccb2899d897206802c5dac39d01f77c3c1392
Mancuerna <jacortesibanez@gmail.com>
2017-07-12 13:55:09 +0200

    Added POST and proper GET.

--- api/correlation.py
+++ api/correlation.py
@@ -3,17 +3,12 @@ from config.config import turtle_folder, jsonld_folder
 
 
 def execute(x, y, method):
-    locals()['dataset'] = '/home/jacortes/PycharmProjects/openccml/wrapperR/mtcars.csv'
     result = wrapperv2.core(locals(), "cor")
     result.cor()
-    """
-    file = result.parameter.getOutput()
-    with open(file) as pmml:
-        return pmml.read()
-    """
+
 def execute_post(use):
-    data = "no data here"
-    return {'data': data}
+    result = wrapperv2.core(locals(), "cor")
+    result.cor()
 
 
 def download(file_format):
