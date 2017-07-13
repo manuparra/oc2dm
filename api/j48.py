0a5083afd197ff4eeda125792c560eace289894f
Mancuerna <jacortesibanez@gmail.com>
2017-07-13 20:10:23 +0200

    Entry point for "J48".

---
+++ api/j48.py
@@ -0,0 +1,33 @@
+from wrapperR import wrapperv2
+from config.config import turtle_folder, jsonld_folder
+
+
+def execute(na__action, data, formula):
+    print(locals())
+    result = wrapperv2.core(locals(), "j48")
+    result.j48()
+    file = result.parameter.getOutput()
+
+
+def execute_post(na__action, data, formula):
+    result = wrapperv2.core(locals(), "j48")
+    result.j48()
+    file = result.parameter.getOutput()
+
+
+def download(file_format):
+    if 'turtle' in file_format:
+        with open(turtle_folder + "/j48.ttl") as file:
+            return file.read()
+    else:
+        with open(jsonld_folder + "/j48.jsonld") as file:
+            return file.read()
+
+
+def download_post(file_format):
+    if 'turtle' in file_format:
+        with open(turtle_folder + "/j48.ttl") as file:
+            return file.read()
+    else:
+        with open(jsonld_folder + "/j48.jsonld") as file:
+            return file.read()
